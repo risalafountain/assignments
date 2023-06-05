3682f8909610d6c2b7d5d82cea79a2e25f572797
risalafountain <risa.garza@outlook.com>
2023-06-05 12:19:04 -0500

    forms 2 notes

---
+++ 99-forms-pt-2/forms2.js
@@ -0,0 +1,29 @@
+const form = document.myForm
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    // Radio Inputs
+    // console.log(form.gender.value)
+    
+    
+    // Checkbox Inputs
+    // console.log(form.entertainment)
+    
+    // const checkedInputs = []
+    
+    // for(let i = 0; i < form.entertainment.length; i++){
+    //     if(form.entertainment[i].checked){
+    //         checkedInputs.push(form.entertainment[i].value)
+    //     }
+    // }
+    
+    // console.log(checkedInputs)
+    
+    
+    // Select Option inputs
+    
+    console.log(form.city.value)
+    
+    
+})
