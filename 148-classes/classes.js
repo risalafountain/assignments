7e2d82c4a59cb918020c1b84ec77a78e0656291e
risalafountain <risa.garza@outlook.com>
2023-07-06 12:06:44 -0500

    studying

--- 148-classes/classes.js
+++ 148-classes/classes.js
@@ -27,23 +27,23 @@
 
 //after the constructor but before the class ends is where we put the methods/functions
 
-class Car {
-    constructor(make,model){
-        this.make = make 
-        this.model = model
-    } 
-    honk(){
-        console.log("honk")
-    }
-    drive(){
-        console.log("vroooooom")
-    }
-}
+// class Car {
+//     constructor(make,model){
+//         this.make = make 
+//         this.model = model
+//     } 
+//     honk(){
+//         console.log("honk")
+//     }
+//     drive(){
+//         console.log("vroooooom")
+//     }
+// }
 
-const ford = new Car ("ford", "bronco")
-console.log(ford.make) // ford
-ford.honk() // honk
-ford.drive()// vroooooom
+// const ford = new Car ("ford", "bronco")
+// console.log(ford.make) // ford
+// ford.honk() // honk
+// ford.drive()// vroooooom
 
 //some vehicles will have prpoerties that others won't 
 
@@ -79,4 +79,31 @@ class Vehicle {
     //         console.log("Vrooom")
     //     }
     // }
-//better than to create the motorcycle class and 
+
+//in order to setup Motorcycle to inherit the properties of the vehicle class, use the word EXTEND. This means that anytime we create a motorcycle object, it will automatically inherit the vehicle properties.  
+/*
+how to read it:
+step 1: anything tied to the motorcycle object automatically has access to the vehicle methods of honk() and drive() because of extend
+step 2: the constructor for motorcycle is similar to the constructor for make and model except it has the addition of hasSideCar. since hasSideCar is specific to Motorcyle, we stillneed to instruct the constructor on how to apply that my adding this.hasSideCar = hasSideCar everything else that is already in the constructor for Vehicle, then use SUPER
+*/
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, hasSideCar) {
+        super (make, model)
+        this.hasSideCar = hasSideCar
+    }
+}
+
+const jeep = new Vehicle("jeep", "cherokee")
+const harley = new Motorcycle("harley", "davidson", true) //hasSideCar =  true (yes)
+
+harley.honk() 
+
+//when you call honk on the harley object, JS checks the Motorcycle blueprint. if it doesn't see the honk method inside the prototype for motorcycle so it then looks at the super class, sees that it does exist, and calls it just the same.   
+
+//start general and get more indepth as the need arises 
+
+//classes can extend other classes to keep the code dry 
+
+
+
