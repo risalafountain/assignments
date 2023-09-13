69cf4cdb6102d5424dd762d1009d58819a07f749
risalafountain <risa.garza@outlook.com>
2023-09-12 20:40:35 -0500

    need to speak to instructor re: 2nd challenge. It shouls work but is still returning undefined

--- 3.5-mid-level-assessment/3-mid.js
+++ 3.5-mid-level-assessment/3-mid.js
@@ -7,60 +7,32 @@
 //return new arr
 
 
-// filter method to pull out all non -1 values
-function solution(arr) {
-    const filteredArr = arr.filter(function (num) {
-        if (num !== -1) {
-            return num
-        }
-    })
-    filteredArr.sort(function (a,b) {
-        return a-b
-    }) 
-    filteredArr.splice[0] //starting at the index of 0 
-}
-
-// fitler creates a new arr
-
-// map thru your original array and if the value !== -1 you can set that value to filteredARR[0]
-
-//splice out filteredArr[0]
-
-// [-1, 150, 160, 170, -1, -1, 180, 190]
-
-// [ ] // sort
-
-
-// filter
-
-//sort your filtered
-
-// map and replace values
-
-
-
-
-
-// function solution(arr){
-//     arr.map((num) => {
-//         if (num === -1) {
-//             console.log('num:', num)
-//     }
-//         else {
-//             arr.sort(function(a, b){
-//                 return a - b
-//             }
-//     )}
+// //#1--filter method to pull out all non -1 values and put them in a new arr
+// function solution(arr) {
+//     const filteredArr = arr.filter(function (num) {
+//         if (num !== -1) {
+//             return num
+//         }
+//        // console.log(num) //-1, -1, -1
 //     })
-//     // const result = arr.sort (function (a,b) {
-//     //     return a-b
-//     // })
-//     //return result
+//     //console.log(filteredArr) //[ 150, 190, 170, 160, 180 ]
+// //#2--sort the new arr 
+//     filteredArr.sort(function (a,b) {
+//         return a-b
+//     }) 
+//     //console.log(filteredArr) //[ 150, 160, 170, 180, 190 ]
+// //#3--map through original arr 
+// //#4--remove numbers from filtered arr to original arr
+//     return arr.map (num => {
+//         if (num !== -1) {
+//             num = filteredArr[0]
+//             filteredArr.splice(0, 1) 
+//         }
+//         return num
+//     })
 // }
 
-
-
-console.log(solution([-1, 150, 190, 170, -1, -1, 160, 180]))
+// console.log(solution([-1, 150, 190, 170, -1, -1, 160, 180]))
 // console.log(solution([5, 3, 1]))
 // console.log(solution([-1, -1, -1, -1]))
 // console.log(solution([100, -1, 50, -1, 75]))
@@ -71,24 +43,47 @@ console.log(solution([-1, 150, 190, 170, -1, -1, 160, 180]))
 
 // Implement the **`countVowels`** function using either the provided example solutions or your own solution.
 
-//-step 1: isolate the vowels
-//-step 2: count the vowels
+//-step : isolate the vowels
+    //make all letters one case 
+//-step : 
+//-step : count the vowels
 
+//convert string to an arr to use arr methods map
+   // const onlyVowels = string.map (function (word) {
 
-// function countVowels(arr) {
-//     let count = 0
-//     //const vowels = ["a", "e", "i", "o", "u" ]
-//     const onlyVowels = arr.map (function (word) {
-//         if (word.charAt(i) === "a" ) {
-//            count ++
-//         }
-//     })
-//     return ("result:" + onlyVowels)
-// }
+function countVowels(string) {
+    let count = 0
+    const vowels = ["a", "e", "i", "o", "u" ]
+//make entire string one case and turn into array of characters
+    const lowerCaseResult=string.toLowerCase()
+//make string an array ( so i can use map method) 
+    const arrResult = [...lowerCaseResult]//spread operator made my string an array of characters!!! yay!
+    console.log(lowerCaseResult) //hello, world!
+    console.log(arrResult)//  ['h', 'e', 'l', 'l','o', ',', ' ', 'w','o', 'r', 'l', 'd','!']
+// go through letters in the string to see if any of the characters are vowels if so add to count
+    arrResult.map(function (letter) {
+        if (vowels.includes(letter)) {
+            count ++
+        }
+        return count
+    })
+}
 
-// const input = 'Hello, World!';
-// console.log(countVowels(["hello World", "cat", "dog", "tree"])); // Output: 3
+//THIS IS STILL RETURNING UNDEFINED!!!! HELP
+
+const input = 'Hello, World!';
+console.log(countVowels(input)); // Output: 3
 
 // const input = 'Counting Vowels';
-
 // console.log(countVowels(input)); // Output: 5
+
+//  })
+  
+// if (charAt[i] === vowels ) {
+//      count ++
+//     }
+// return count
+// // console.log (`result ${onlyVowels}`) 
+// return word
+//     //("result:" + onlyVowels)
+// }
