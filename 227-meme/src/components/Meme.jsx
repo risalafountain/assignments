69b119921f854fffbbc5d004795d4a902db90365
risalafountain <risa.garza@outlook.com>
2023-10-07 18:28:21 -0500

    10.7.23

--- 227-meme/src/components/Meme.jsx
+++ 227-meme/src/components/Meme.jsx
@@ -3,23 +3,32 @@ import memesData from "./memesData";
 
 
 export default function Meme(){
-    const [memeImage, setMemeImage] = React.useState("")
+    const [memeImage, setMemeImage] = React.useState({
+        topText: "", 
+        bottomText: "", 
+        randomImage:"../images/default.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage(){
         // access the arr and get an object
-        const memesArray = memesData.data.memes
-        
+        const memesArray = allMemeImages.data.memes
         // randomize Selection
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         // console.log(randomNumber)
         
         // get image object by index into meme as array and index of  random number
-        setMemeImage(memesArray[randomNumber].url) 
+        const url = memesArray[randomNumber].url 
         //console.log(url)//this is randomizing! it works
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
     return(
         <main>
-            {/*put the form in a div instead of a form tag because since there is a button, it will refresh the page unless we stop it */}
+            {/*put the form in a div instead of a form tag because  there is a button, which will refresh the page unless we stop it */}
             <div className="form">
                 <input 
                 type="text"
@@ -37,39 +46,8 @@ export default function Meme(){
                     > 
                     Get a new meme image  🖼
                     </button>
-                <img src={memeImage} className="meme--image" />
+                <img src={meme.RandomImage} className="meme--image" />
             </div>
         </main>
     )
 }
-
-/**235.75-Add Images to the Meme Generator
-
-* Challenge: Save the random meme URL in state
-* - Create new state called `memeImage` with an empty string as default
-* - When the getMemeImage function is called, update the `memeImage` state to be the random chosen
-*   image URL * - Below the div.form, add an <img /> and set the src to the new `memeImage` state you created
-*add some styling to make sure the image stays within its boundaries
-*/        
-
-/**project startup
- * Challenge: 
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- * 
- * best to layout using grid in css
- * 
- *     function getMemeImage() {
-        // access the arr and get an object
-        const memesArray = memesData.data.memes
-        // randomize Selection
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        // console.log(randomNumber)
-        // get image object by index into memeas array and index of random number
-        const url = memesArray[randomNumber].url //this line is incomplete!
-        //console.log(url)
-    }
-
- */
--- 227-meme/src/components/notes.md
+++ 227-meme/src/components/notes.md
@@ -39,7 +39,69 @@ GET RANDOM MEME--229.75
      */
 
 OUR CURRENT CONUNDRUM--see 230-conundrum
+/**235.75-Add Images to the Meme Generator
+
+* Challenge: Save the random meme URL in state
+* - Create new state called `memeImage` with an empty string as default
+* - When the getMemeImage function is called, update the `memeImage` state to be the random chosen
+*   image URL * - Below the div.form, add an <img /> and set the src to the new `memeImage` state you created
+*add some styling to make sure the image stays within its boundaries
+*/        
+
+/**project startup
+ * Challenge: 
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ * 
+ * best to layout using grid in css
+ * 
+ *     function getMemeImage() {
+        // access the arr and get an object
+        const memesArray = memesData.data.memes
+        // randomize Selection
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        // console.log(randomNumber)
+        // get image object by index into memeas array and index of random number
+        const url = memesArray[randomNumber].url //this line is incomplete!
+        //console.log(url)
+    }
+
+ */
+
+
+239.25-REFACTOR STATE 
+* Challenge: Update our state to save the meme-related data as an object called `meme`. It should have the following 3 properties:
+     * topText, bottomText, randomImage.
+
+OLD VERSION :
+    const [memeImage, setMemeImage] = React.useState("")
+
+UPDATED VERSION:
+--use object instead of string 
+    const [memeImage, setMemeImage] = React.useState({})
+
+--put in the three properties we want in curly braces 
+    const [memeImage, setMemeImage] = React.useState({
+        topText: "", 
+        bottomText: "", 
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+--another state var to access all meme images--initial value will be our memes data arr
+--anywhere that we were using memesData, replace with all MemeImages
+
+The 2 text states can default to empty strings for now, amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+
+Next, create a new state variable called `allMemeImages`
+which will default to `memesData`, which we imported above
+
+Lastly, update the `getMemeImage` function and the markup 
+to reflect our newly reformed state object and array in the
+correct way.
 
 
 
- 
+    
+
