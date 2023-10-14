31cff8d4f7b09abc44a6e29d273c8ba5cc26cf05
risalafountain <risa.garza@outlook.com>
2023-10-14 10:37:50 -0500

    101323

--- 245-Dice/src/App.jsx
+++ 245-Dice/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import DiceBox from './components/DiceBox'
 
@@ -19,6 +17,7 @@ function App() {
             <h2> this is the dicebox component in APP </h2>
             <DiceBox />
         </div> */}
+        <h1>app.jsx works</h1>
     </main>
   )
 }
--- 245-Dice/src/components/Die.jsx
+++ 245-Dice/src/components/Die.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function Die(){
-    
+export default function Die(props){
+    return (
+    <div className="die--number">
+        <h1>{props.die}</h1>
+    </div>
+    )
 }
