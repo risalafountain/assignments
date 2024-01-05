87065a85b3ed1d7d329736600407cbfce8f8a983
risalafountain <risa.garza@outlook.com>
2024-01-05 05:49:29 -0600

    color gradient app completed.

--- 286-color-gradient/src/App.jsx
+++ 286-color-gradient/src/App.jsx
@@ -11,42 +11,80 @@ function App() {
   // })
 
   const [angle, setAngle] = useState("50deg", "90deg", "180deg")
-  const [color1, setColor1] = useState('pink')
-  const [color2, setColor2] = useState('purple')
+  const [color1, setColor1] = useState('#FF0000')
+  const [color2, setColor2] = useState('#0000FF')
+  // const [preview, setPreview] = useState({
+  //   angle: "angle",
+  //   color1: "color1",
+  //   color2: "color2"
+  // }
+  // )
 
   // each radio input needs a handle change to save the change and set state
-  handleChange(){
+  function handleChange(event) {
+    const { name, value } = event.target
+    console.log(event.target.value)
 
+    // setAngle((prevAngle) => ({
+    //   [name] : value
+    // }))
+
+
+    // if/else statements and set state 
+    if (name === "color1") {
+      // console.log({color1})
+      setColor1(value)
+
+    } else if
+      (name === "color2") {
+      setColor2(value)
+
+    } else if
+      (name === "angle") {
+      setAngle(value)
+
+      setPreview(prevPreview => ({
+        ...prevPreview,
+        [name]: value
+      })
+      )
+    }
   }
+
+
 
   //for the button
-  handleSubmit(){
+  // handleSubmit( ){
+  // }
 
-  }
   return (
     <main className='form'>
-      <div className='gradient-preview'>
+      <div className='gradient-setup'>
         <h1>Color Gradient Generator</h1>
 
         <div className='colors'>
-          <h2>Color 1</h2>
+          <h2>Color 1: {color1}</h2>
           <input
             type="color"
             className='color'
             name="color1"
             // value={gradient.color1}
             value={color1}
+            onChange={handleChange}
+
           />
         </div>
 
         <div className='colors'>
-          <h2>Color 2</h2>
+          <h2>Color 2: {color2}</h2>
           <input
             type="color"
             className='color'
             name="color2"
             // value={gradient.color2}
             value={color2}
+            onChange={handleChange}
+
           />
         </div>
 
@@ -55,10 +93,22 @@ function App() {
           <li>
             <input
               type="radio"
+              name="angle"
+              // value={gradient.angle}
+              value={"50deg"}
+              onChange={handleChange}
+            />
+            50 deg
+          </li>
+          <li>
+            <input
+              type="radio"
               // if they all have the same name only one can be selected at a time
               name="angle"
               // value={gradient.angle}
               value={"90deg"}
+              onChange={handleChange}
+
             />
             90 deg
           </li>
@@ -68,31 +118,35 @@ function App() {
               name="angle"
               // value={gradient.angle}
               value={"180deg"}
+              onChange={handleChange}
             />
             180 deg
           </li>
-          <li>
-            <input
-              type="radio"
-              name="angle"
-              // value={gradient.angle}
-              value={"50deg"}
-            />
-            50 deg
-          </li>
+       
 
         </div>
 
 
         <div className='preview'>
-          <h1>Preview</h1>
+          <h1 >Preview</h1>
           {/* generate preview by passing in the colors and angle selected  */}
-          {/* {gradient?}  */}
-          <button>Generate Preview</button>
-          {/* how do i display the preview here? create an empty div?  */}
-          <textarea className='text-code'>
+          
+          <div className='preview-box'style={{ background: `linear-gradient(${angle}, ${color1}, ${color2})` }}>
+         
+            {/* <textarea className='text-code'>
             this is the text area which will generate the css code
-          </textarea>
+          </textarea> */}
+
+          </div>
+          <p>Copy CSS Code below:</p>
+          <hr/>
+          <textarea readOnly 
+          className="text-code" 
+          value= {`background: linear-gradient(${angle}, ${color1} , ${color2}); \n-moz-background:linear-gradient(${angle}, ${color1} , ${color2});\n-webkit: linear-gradient(${angle}, ${color1} , ${color2})`} />
+          {/* {gradient?}  */}
+          {/* <button>Generate Preview</button>
+          how do i display the preview here? create an empty div?  */}
+
         </div>
       </div>
 
