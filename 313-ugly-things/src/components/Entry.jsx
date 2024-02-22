8e034cc5436b2bb8f53ecdb27cb9fb14d7e2e04a
Risa La Fountain <risa.garza@outlook.com>
2024-02-22 16:27:55 -0600

    created Entry component

--- 313-ugly-things/src/components/Entry.jsx
+++ 313-ugly-things/src/components/Entry.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 export default function Entry(){
-   function handleEdit
-   
+ 
+    function handleEdit() {
+        
+    }
     return (
-     <p>Title: {entry.title}</p>
-    <img src={entry.imgUrl} style={{ width: '150px', height: '150px' }} />
-    <p>Reason: {entry.description}</p>
-    <p>{entry.id}</p>
-    <button className='form--button' onClick={() => handleEdit(entry._id)}>edit</button>
-    <button className='form--button' onClick={() => handleDelete(entry._id)} > delete</button
-   )
-   >
+        <div>
+<p> Title: {entry.title}</p>
+<img 
+src ={entry.imgUrl}
+style={{width: '150px', height: '150px' }}
+/>
+
+<p>Reason: {entry.description}</p>
+<p>{entry.id}</p>
+<button className="form--button" onClick={() => handleEdit(entry.id)} > Edit </button>
+<button className="form--button" onClick={() => handleDelete(entry.id)}>Delete </button>
+        </div>
+    )
 }
+   
