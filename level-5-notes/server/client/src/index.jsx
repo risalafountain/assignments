1241c9dbae91b5c55d7dfcf16de002f631d87194
Risa LaFountain <risa.garza@outlook.com>
2024-04-02 00:12:20 -0500

    imported css to index.jsx

--- level-5-notes/server/client/src/index.jsx
+++ level-5-notes/server/client/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
 import App from "./App"
+import './styles.css'
 
 
+
+//i am getting an error for using this since react18 is different but I cannot determine how to change it 
 ReactDOM.render(<App/>, document.getElementById('root'))
