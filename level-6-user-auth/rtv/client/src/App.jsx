ea6462127f6e3d533c57a3fb941a1b22026fe727
Risa LaFountain <risa.garza@outlook.com>
2024-05-01 22:40:38 -0500

    fixing errs in console

--- level-6-user-auth/rtv/client/src/App.jsx
+++ level-6-user-auth/rtv/client/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+// auto import does not add .jsx...is this ok?
+import React, {useContext} from 'react'
+import { UserContext } from './context/UserProvider'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Auth from './components/Auth'
+import Profile from './components/Profile'
+import Public from './components/Public'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="app">
-    <Navbar logout={logout} />
+    {/* <Navbar logout={logout} /> */}
     <Routes>
       <Route 
         path="/" 
--- level-6-user-auth/rtv/client/src/main.jsx
+++ level-6-user-auth/rtv/client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import UserProvider from './context/UserContext.jsx'
+import UserProvider from './context/UserProvider.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
